0a4ec364c95ce2cb744ecca24806101a0c39e296
e-RicardoGama <92930492+e-RicardoGama@users.noreply.github.com>
2025-10-21 15:00:56 -0300

    sem sw

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,35 @@
+// layout.tsx - VERSÃO SIMPLIFICADA
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { AuthProvider } from '../context/AuthContext';
+import { Suspense } from 'react';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: "Nutri",
+  description: "Fotografe seu prato e receba uma análise nutricional completa com inteligência artificial.",
+  icons: {
+    icon: '/imagens/alimentacao.jpg',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <Suspense fallback={<div>Carregando...</div>}> 
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </Suspense>
+        
+      </body>
+    </html>
+  );
+}
