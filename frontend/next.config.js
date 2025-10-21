d2d79ed50be0e36dbdbc134e049d03b39324fc95
e-RicardoGama <92930492+e-RicardoGama@users.noreply.github.com>
2025-10-21 19:13:03 -0300

    novo deploy do zero

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
       },
     ]
   },
+  eslint: {
+    // Aviso: Isso vai permitir que o build seja concluído 
+    // mesmo que seu projeto tenha erros de ESLint.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
