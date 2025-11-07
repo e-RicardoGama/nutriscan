493b6fa3e94420578d12d4c55995d3d3bb173787
e-RicardoGama <92930492+e-RicardoGama@users.noreply.github.com>
2025-11-07 17:35:15 -0300

    crud

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -4,7 +4,7 @@ from sqlalchemy import func, cast, Date
 from typing import Optional, List, Dict, Any
 import json
 from datetime import datetime,date
-from pytz import timezone
+from zoneinfo import ZoneInfo
 
 # --- Imports Explícitos ---
 from app.models.refeicoes import RefeicaoSalva, AlimentoSalvo, RefeicaoStatus
@@ -128,7 +128,7 @@ def get_refeicoes_hoje_por_usuario(db: Session, user_id: int) -> List[RefeicaoSa
     Busca todas as refeições do usuário de hoje
     que já tiveram a análise concluída.
     """
-    today = datetime.now(timezone("America/Sao_Paulo")).date()
+    today = datetime.now(ZoneInfo('America/Sao_Paulo')).date()
 
     return db.query(RefeicaoSalva).filter(
         RefeicaoSalva.owner_id == user_id,
